3db633c4982c8520b96703e452c7536c7502615d
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-05-07 15:47:08 -0300

    Initial commit

---
+++ Assets/SumadorUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class SumadorUI : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    public TMP_InputField inputNum1;
+    public TMP_InputField inputNum2;
+    public TextMeshProUGUI textResultado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textResultado.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SumarYMostrarResultado()
+    {
+        //tomar los valores del input y sumarlos
+        num1 = int.Parse(inputNum1.text);
+        num2 = int.Parse(inputNum2.text);
+
+        textResultado.text = $"{num1 +num2}";
+    }
+
+}
