8905ea1576b5921864077c237250aa1b1a94d76c
millieloisi <167239633+millieloisi@users.noreply.github.com>
2025-05-14 16:00:47 -0300

    Par o Impar

---
+++ Assets/imparopar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class imparopar : MonoBehaviour
+{
+    public int num1;
+    public TMP_InputField numero;
+    public TextMeshProUGUI textResultado;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        textResultado.text = "";
+
+
+    }
+
+    public void EsParOimpar()
+    {
+        if (numero.text == "") 
+        {
+            textResultado.text = "El campo esta vacio";
+            return;
+        }
+        num1 = int.Parse(numero.text);
+
+        if (num1 <= 0)
+        {
+            textResultado.text = "El nomero debe ser mayor que 0";
+            return;
+        }
+
+        if (num1 % 2 == 0)
+        {
+            textResultado.text = "El numero es par";
+            
+        }
+
+        else
+        {
+            textResultado.text = "El numero es impar";
+            
+        }
+    }
+   
+
+}
